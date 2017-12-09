c5f49877856ee0280e49a3dc866067b1f8b6cfa1
Alx101 <alex1000209@gmail.com>
2017-12-09 17:14:01 +0100

    Added fail and success routes for page registration. Hooked up receipt fetching

--- App/routes.php
+++ App/routes.php
@@ -7,16 +7,18 @@ use App\Controllers\CustomerController;
 // Routes
 
 $app->get('/', function (Request $request, Response $response) {
-    // Sample log message
-    //$this->logger->info($this->db->lastErrorMsg());
-
-    // Render index view
     return $this->renderer->render($response, 'index.phtml');
 });
 
 $app->get('/registercard/{cid}', function (Request $request, Response $response, $args) {
     //ob_start();
     $customer = Customer::where('cid', $args['cid'])->first();
+
+    if(!$customer) {
+        $response->getBody()->write("Customer not found!");
+        return $response;
+    }
+
     $service_url = 'http://zeipt.io/verifone/RegisterCard/';
     $username = 'alex';
     $password = 'zeipt.com';
@@ -53,6 +55,36 @@ $app->get('/registercustomer/{name}', function (Request $request, Response $resp
     }
 });
 
-$app->get('/test', function(Request $request, Response $response) {
-    return;
+$app->post('/SuccessPage', function (Request $request, Response $response) {
+    return $this->renderer->render($response, 'success.phtml');
+});
+
+$app->post('/FailPage', function (Request $request, Response $response) {
+    return $this->renderer->render($response, 'fail.phtml');
+});
+
+$app->get('/receipts/{gcid}', function(Request $request, Response $response, $args) {
+    $customer = Customer::where('cid', $args['cid'])->first();
+    if(!$customer) {
+        $response->getBody()->write("Customer not found!");
+        return $response;
+    }
+
+    $service_url = 'http://zeipt.io/verifone/ReceiptZeipt/';
+    $username = 'alex';
+    $password = 'zeipt.com';
+    $curl = curl_init($service_url);
+    $curl_post_data = array(
+        'GCI' => $customer->cid
+    );
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
+    curl_setopt($curl, CURLOPT_USERPWD, "$username:$password");
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+    $curl_response = curl_exec($curl);
+
+    curl_close($curl);
+    $response->getBody()->write($curl_response);
+    return $response;
 });
